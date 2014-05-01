b858764a800fa4f36cea8173012fa9c6b0be05e6
cultpenguin <cultpenguin@50e6ce60-8686-46c6-9475-e6d7974e1d10>
2014-05-01 14:15:55 +0000

    New docs

---
+++ doc/MakeXmlRef.m
@@ -0,0 +1,39 @@
+i=0;
+i=i+1;f{i}='.';
+i=i+1;f{i}=['toolboxes',filesep,'traveltime'];
+%i=i+1;f{i}='sgems/*.m';
+%i=i+1;f{i}='snesim/*.m';
+%i=i+1;f{i}='misc/*.m';
+%i=i+1;f{i}='fast/*.m';
+
+
+for ff=1:length(f)
+    
+    FILES=dir(['..',filesep,f{ff},filesep,'*.m']);
+    if ff==1
+        name='sippi_functions.xml';
+    
+    else
+        name=sprintf('sippi_functions_%s.xml',space2char(f{ff},'_',filesep));
+
+    end
+    fid=fopen(name,'w');
+    
+    for i=1:length(FILES)
+      [p,name,ext]=fileparts(FILES(i).name);
+      disp(name)
+      h=help(name);
+      
+      %fprintf(fid,'<sect2 id=\"%s\"><title>%s</title>\n',name,name);
+      fprintf(fid,'<sect2 xml:id=\"%s\"><title>%s</title>\n',name,name);
+      fprintf(fid,'<para><programlisting><![CDATA[%s]]></programlisting></para>\n',h);
+      fprintf(fid,'</sect2>\n\n');
+      
+    end
+
+
+    fclose(fid);
+
+    
+end
+  
