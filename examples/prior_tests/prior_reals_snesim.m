4fd2b2ec26f96e316562b85756c757a1c7bcc8c0
cultpenguin <cultpenguin@50e6ce60-8686-46c6-9475-e6d7974e1d10>
2014-06-06 12:36:31 +0000

    new examples of running snesim

--- examples/prior_tests/prior_reals_snesim.m
+++ examples/prior_tests/prior_reals_snesim.m
@@ -1,30 +1,24 @@
 % prior_reals_snesim Sampling a SNESIM type prior model
 clear all,
-im=1; 
-prior{im}.type='SNESIM';
-prior{im}.x=[0:.1:10]; % X array 
-prior{im}.y=[0:.1:20]; % Y array 
+ip=1; 
+prior{ip}.type='SNESIM';
+prior{ip}.x=[0:.1:10]; % X array 
+prior{ip}.y=[0:.1:20]; % Y array 
 
 %% get Training image from file
 % f_ti='3194256336_e75579cd07_m.sgems';
 % %f_ti='2485773707_997e223aab_n.sgems';
 % O=sgems_read(f_ti);
 % ti=O.D(:,:,:,3);figure(1);;imagesc(ti);axis image;colorbar;title('ti')
-% prior{im}.ti=ti;
+% prior{ip}.ti=ti;
 % %
-% prior{im}.index_values=[0 1 2];
-% prior{im}.m_values=[8 12 10];
+% prior{ip}.index_values=[0 1 2];
+% prior{ip}.m_values=[8 12 10];
 
-
-prior{im}.S=sgems_get_par('snesim_std');
-prior=sippi_prior_init(prior);
-
-prior{im}.scaling=.7;
-prior{im}.rotation=30;
 
 randn('seed',1);rand('seed',1);
 
-%% UNCONDITIONAL
+%% UNCONDITIONAL NO SCALING
 figure(10);clf
 for i=1:5;
     m=sippi_prior(prior);
@@ -40,11 +34,45 @@ print_mul('prior_reals_snesim');
 suptitle(sprintf('Independant samples from a SNESIM type prior'))
 
 
+%% 
+figure(11);clf
+[m,prior]=sippi_prior(prior);
+for i=1:5;
+    prior{ip}.S.XML.parameters.Nb_Multigrids_ADVANCED.value=i;
+    m=sippi_prior(prior);
+	subplot(1,5,i);
+    imagesc(prior{1}.x,prior{1}.y,m{1});axis image	
+    title(sprintf('NM GRIDS = %d',i))
+end
+colormap(sippi_colormap(1));
+colorbar_shift;
+print_mul('prior_reals_snesim_nmgrid');
 
+
+%% UNCONDITIONAL WITH SCALING
+figure(12);clf
+prior{ip}.scaling=.7;
+prior{ip}.rotation=30;
+for i=1:5;
+    m=sippi_prior(prior);
+    subplot(1,5,i);
+    imagesc(prior{1}.x,prior{1}.y,m{1});
+    axis image
+    %xlabel('X')
+    %ylabel('Y')
+end
+colormap(sippi_colormap(1));
+colorbar_shift;
+print_mul('prior_reals_snesim_rotation_scale');
+suptitle(sprintf('Independant samples from a SNESIM type prior'))
+
+
+
+return
 %% SEQ SIM 1
 randn('seed',1);rand('seed',1);
-prior{im}.seq_gibbs.type=1;
-prior{im}.seq_gibbs.step=[20];
+prior{ip}.seq_gibbs.type=1;
+prior{ip}.seq_gibbs.step=[20];
 [m,prior]=sippi_prior(prior);
 [m,prior]=sippi_prior(prior,m);  
 figure(11);clf
@@ -65,8 +93,8 @@ suptitle(sprintf('Sampling from a SNESIM type prior\nSequential Gibbs sampling')
 
 %% SEQ SIM 2
 randn('seed',1);rand('seed',1);
-prior{im}.seq_gibbs.type=2;
-prior{im}.seq_gibbs.step=.97;
+prior{ip}.seq_gibbs.type=2;
+prior{ip}.seq_gibbs.step=.97;
 [m,prior]=sippi_prior(prior);
 [m,prior]=sippi_prior(prior,m);  
 figure(12);clf
