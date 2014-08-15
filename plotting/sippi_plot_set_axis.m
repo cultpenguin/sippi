90ad1d7f0f80d8c2b3293144c7bc329a4914f464
cultpenguin <cultpenguin@50e6ce60-8686-46c6-9475-e6d7974e1d10>
2014-08-15 09:38:17 +0000

    warpper for settting the axis in a consistent manner

---
+++ plotting/sippi_plot_set_axis.m
@@ -0,0 +1,18 @@
+% sippi_plot_set_axis
+% see also sippi_plot_defaults
+
+function sippi_plot_set_axis(options)
+if nargin==1
+    options=sippi_plot_defaults(options);
+else
+    options=sippi_plot_defaults;
+end    
+
+%ppp(options.plot.axis.width,options.plot.axis.height,options.plot.axis.fontsize,options.plot.axis.w0,options.plot.axis.h0);
+            
+set(gca,'FontSize',options.plot.axis.fontsize)
+
+set(get(gca,'Xlabel'),'FontSize',options.plot.axis.fontsize-2)
+set(get(gca,'Ylabel'),'FontSize',options.plot.axis.fontsize-2)
+set(get(gca,'Zlabel'),'FontSize',options.plot.axis.fontsize-2)
+
