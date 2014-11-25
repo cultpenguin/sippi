fa8daf0724cadaabdf3185bcbbe5097dcdf64ae0
cultpenguin <cultpenguin@50e6ce60-8686-46c6-9475-e6d7974e1d10>
2014-11-25 13:04:20 +0000

    Misc updates (cosmetic)

--- trunk/examples/prior_tests/prior_reals_fftma_cov.m
+++ trunk/examples/prior_tests/prior_reals_fftma_cov.m
@@ -5,12 +5,13 @@ clear all;
 try;close(12);end
 try;close(13);end
 
+%% Setup FFT-MA type a priori model with uncertainty covariance model parameters
 im=0;
 
-im=im+1; 
+im=im+1;
 prior{im}.type='FFTMA';
-prior{im}.x=[0:.1:10]; % X array 
-prior{im}.y=[0:.1:20]; % Y array 
+prior{im}.x=[0:.1:10]; % X array
+prior{im}.y=[0:.1:20]; % Y array
 prior{im}.m0=10;
 prior{im}.Cm='1 Sph(10,90,.25)';
 
@@ -25,15 +26,15 @@ im=im+1;
 prior{im}.type='gaussian';
 prior{im}.name='ang_1';
 prior{im}.m0=90;
-prior{im}.std=10;
+prior{im}.std=30;
 prior{im}.prior_master=1;
 
 
-%%
+%% Plot sample of the prior model
 randn('seed',4);
 figure(12);clf
 for i=1:5;
-    m=sippi_prior(prior);
+    [m,prior]=sippi_prior(prior);
     subplot(1,5,i);
     imagesc(prior{1}.x,prior{1}.y,m{1});
     %caxis([8 12])
@@ -47,15 +48,14 @@ print_mul('prior_reals_fftma_cov')
 suptitle(sprintf('FFT-MA with varying covariance properties'))
 
 
-%% SEQ GIBBS
-% 
+%% Plot 5 models generated using seqential Gibbs sampling
 randn('seed',4);rand('seed',4);
 figure(13);clf
 prior{2}.seq_gibbs.step=0;.5;
 prior{3}.seq_gibbs.step=0;.25;
 
-prior{1}.seq_gibbs.type=1;;
-prior{1}.seq_gibbs.step=5;;
+prior{1}.seq_gibbs.type=1;
+prior{1}.seq_gibbs.step=5;
 prior{1}.perturb=1;
 [m,prior]=sippi_prior(prior);
 for i=1:5;
@@ -73,35 +73,37 @@ print_mul('prior_reals_fftma_cov_seqgibbs')
 suptitle(sprintf('FFT-MA with varying covariance properties\n Sequential Gibbs sampling'))
 
 
-%% SEQ GIBBS MOVIE
-% 
+%% Show movie of a random walk in the prior (using sequential Gibbs sampling)
 fclose all;
-close all
+close all;
+save_movie=1;
 randn('seed',4);rand('seed',4);
 fig=figure(13);clf
-prior{1}.seq_gibbs.step=.15;
+prior{1}.seq_gibbs.type=1;
+prior{1}.seq_gibbs.step=5;
 prior{2}.seq_gibbs.step=.15;
-prior{3}.seq_gibbs.step=0.05;;
+prior{3}.seq_gibbs.step=.15;
 prior{3}.perturb=1;
 [m,prior]=sippi_prior(prior);
-aviobj = avifile('prior_reals_fftma_cov_seqgibbs.avi')
+if (save_movie==1)
+    fname=[mfilename,'.mp4'];
+    vidObj = VideoWriter(fname,'MPEG-4');
+    open(vidObj);
+end
 for i=1:250;
     [m,prior]=sippi_prior(prior,m);
     subplot(1,1,1);
-    imagesc(prior{3}.x,prior{3}.y,m{3});
+    imagesc(prior{1}.x,prior{1}.y,m{1});
     caxis([8 12])
     axis image
     %colorbar_shift;
     colormap(sippi_colormap(1));
     drawnow;
-    F = getframe(fig);
-    aviobj = addframe(aviobj,F);
-     %xlabel('X')
-    %ylabel('Y')
+    if save_movie==1
+        % Write each frame to the file.
+        currFrame = getframe;
+        writeVideo(vidObj,currFrame);
+    end
 end
-aviobj = close(aviobj);
- 
-%print_mul('prior_reals_fftma_cov_seqgibbs')
-%suptitle(sprintf('FFT-MA with varying covariance properties\n Sequential Gibbs sampling'))
-
-
+if save_movie==1;try;close(vidObj);end;end
+    
--- trunk/examples/prior_tests/prior_reals_sisim.m
+++ trunk/examples/prior_tests/prior_reals_sisim.m
@@ -1,5 +1,7 @@
 % prior_reals_sisim Sampling a SISIM type prior model
 clear all,
+
+%% Define a 2D SISIM type a priori model
 im=1; 
 prior{im}.type='SISIM';
 prior{im}.x=[0:.1:10]; % X array 
@@ -8,21 +10,17 @@ prior{im}.m0=10;
 prior{im}.Va='1 Sph(10,90,.25)';
 prior{im}.marginal_prob=[0.1 0.2 .4 .3];
 
-
-prior=sippi_prior_init(prior);
-
-randn('seed',1);
-%%
+%%  Plot sample of the prior model
 figure(10);clf
 for i=1:5;
-    m=sippi_prior(prior);
+    [m,prior]=sippi_prior(prior);
     subplot(1,5,i);
     imagesc(prior{1}.x,prior{1}.y,m{1});
     axis image
     %xlabel('X')
     %ylabel('Y')
 end
-try;load cmap;colormap(cmap);end
+colormap(sippi_colormap)
 colorbar_shift;
 print_mul('prior_reals_sisim')
 suptitle(sprintf('A priori realizations from a SISIM type prior model'))
