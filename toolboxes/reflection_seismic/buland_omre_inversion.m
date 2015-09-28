ea5ba219e49bd1a5e530de6ed88fe36fb591b996
cultpenguin <cultpenguin@gmail.com>
2015-09-28 13:25:00 +0200

    slight avo changes

--- toolboxes/reflection_seismic/buland_omre_inversion.m
+++ toolboxes/reflection_seismic/buland_omre_inversion.m
@@ -8,7 +8,11 @@
 % See also: buland_omre_setup
 %
 %
-function  [M,V,vp_est,vs_est,rho_est,Cm_est]=buland_omre_inversion(d,log_vp0,log_vs0,log_rho0,angle,wavelet,Cd,Cm);
+function  [M,V,vp_est,vs_est,rho_est,Cm_est]=buland_omre_inversion(d,log_vp0,log_vs0,log_rho0,angle,wavelet,Cd,Cm,i_traces);
+
+if nargin<9
+    i_traces=1:size(log_vp0,2);
+end
 
 na=length(angle);
 
@@ -21,6 +25,13 @@ end
 ns=nm_all/na;
 disp(sprintf('%s: ns=%g',mfilename,ns))
 
+
+% CONSIDER i_traces
+if nargin<9
+    i_traces=1:nx;
+end
+
+
 % SETUP INVERSION MATRICES
 [A,D,W]=buland_omre_setup(log_vp0,log_vs0,log_rho0,ns,angle,wavelet);
 G=W*A*D;
@@ -30,8 +41,9 @@ m0=[ones(ns,1)*log_vp0;ones(ns,1)*log_vs0;ones(ns,1)*log_rho0];
   
 
 % INVERT
-for ix=1:nx
-  
+
+for iix=1:length(i_traces);
+  ix=i_traces(iix);
   progress_txt(ix,nx,'Buland and Omre linear NMO inversion')
   
   if iscell(d);
@@ -44,16 +56,23 @@ for ix=1:nx
   %[m_est,Cm_est]=least_squares_inversion(G,Cm,Cd,m0,d_obs);
   
   % Much faster 
-  if ix==1;
+  if iix==1;
       S = Cd + G*Cm*G';
       K=(Cm*G')/S;
+      GCm=G*Cm;
   end
   m_est  = m0 + K * (d_obs-G*m0);
-  Cm_est = Cm - K * (G*Cm);
-  
   M(:,ix)=m_est(:);
-  V(:,ix)=diag(Cm_est);
- 
+  
+  % only compute posterior covariance if asked for
+  if nargout>5
+      Cm_est = Cm - K * GCm;
+      V(:,ix)=diag(Cm_est);
+  else
+      V(:,ix)=diag(Cm);
+  end
+  
+  
 end
 
 vp_est=M(1:ns,:);
