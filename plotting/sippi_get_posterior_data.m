e1e6ccb5026cf094b7bb0dfa7332d5bdba2d0ee9
Thomas Mejer Hansen <cultpenguin@gmail.com>
2016-01-10 01:53:42 +0100

    A more consistent way to load posterior realizations

--- plotting/sippi_get_posterior_data.m
+++ plotting/sippi_get_posterior_data.m
@@ -1,4 +1,12 @@
-function [data,prior,options,mcmc]=sippi_get_posterior_data(options);
+% sippi_get_posterior_data: load all data stored in mat-file
+%
+% Call: 
+%
+%  [data,prior,options,mcmc]=sippi_get_posterior_data(folder_name);
+%  [data,prior,options,mcmc]=sippi_get_posterior_data(output_stuct);
+%
+function [data,prior,options,mcmc]=sippi_get_posterior_data(folder_name);
+
 %options,prior,data,forward
 %% LOAD THE CORRECT DATA
 cwd=pwd;
@@ -7,12 +15,12 @@ if nargin==0
     [p,matfile]=fileparts(pwd);
     load(matfile);
 elseif nargin==1;
-    if isstruct(options),
-    else
-        fname=options;
-        cd(fname);
-        load(fname);
+    if isstruct(folder_name);
+        folder_name=folder_name.txt;
     end
+    fname=folder_name;
+    cd(folder_name);
+    load(folder_name);
 end
 if exist('C');
     mcmc=C{1}.mcmc;
