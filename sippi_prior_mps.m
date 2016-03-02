22bafd9e08de26f6a894a297448cb3d212952b9c
Thomas Mejer Hansen <thomas.mejer.hansen@gmail.com>
2016-03-02 22:09:52 +0100

    new documentation of MPS prior

--- sippi_prior_mps.m
+++ sippi_prior_mps.m
@@ -16,6 +16,17 @@
 %    sippi_plot_prior(prior,m)
 %    figure(1);imagesc(prior{ip}.ti);axis image
 %
+%
+%%  The specific algorithm use for MPS simulation is defined in ytje 'method' field
+%    prior{ip}.method='mps_snesim';         % default, same as 'mps_snesim_tree'
+%    prior{ip}.method='mps_snesim_tree';
+%    prior{ip}.method='mps_snesim_list';
+%    prior{ip}.method='mps_enesim_general';
+%
+%    All properties for each algorithm are availale in the prior{ip}.MPS
+%    field
+%
+% See also: sippi_prior, ti, mps_cpp
 
 %% Sequential Gibbs sampling type 1 (box selection of pixels)
 %    prior{ip}.seq_gibbs.type=1;%    
@@ -27,6 +38,7 @@
 %       drawnow;
 %    end
 %
+
 %% Sequential Gibbs sampling type 2 (random pixels)
 %    prior{ip}.seq_gibbs.type=2;%    
 %    prior{ip}.seq_gibbs.step=.6; % Resim 60% of data
@@ -37,7 +49,7 @@
 %       drawnow;
 %    end
 %
-% See also: sippi_prior, ti
+%
 %
 function [m_propose,prior]=sippi_prior_mps(prior,m_current,ip);
 
@@ -61,10 +73,10 @@ if ~isfield(prior{ip},'method')
     % prior{ip}.method='mps_enesim';
 end
 
-prior{ip}.S.method=prior{ip}.method;
-%prior{ip}.S.template_size=[9 9 1];
-prior{ip}.S.nreal=1;
-prior{ip}.S.parameter_filename='mps.txt';
+prior{ip}.MPS.method=prior{ip}.method;
+%prior{ip}.MPS.template_size=[9 9 1];
+prior{ip}.MPS.nreal=1;
+prior{ip}.MPS.parameter_filename='mps.txt';
 
 if prior{ip}.ndim==1;
     SIM=zeros(prior{ip}.dim(1));    
@@ -76,25 +88,25 @@ end
 
 % initialize prior and set the x,y,z dimensions
 prior=sippi_prior_init(prior,ip);
-prior{ip}.S.simulation_grid_size=[length(prior{ip}.x) length(prior{ip}.y) length(prior{ip}.z)];
-prior{ip}.S.origin=[prior{ip}.x(1) prior{ip}.y(1) prior{ip}.z(1)];
-prior{ip}.S.grid_cell_size=[1 1 1];
+prior{ip}.MPS.simulation_grid_size=[length(prior{ip}.x) length(prior{ip}.y) length(prior{ip}.z)];
+prior{ip}.MPS.origin=[prior{ip}.x(1) prior{ip}.y(1) prior{ip}.z(1)];
+prior{ip}.MPS.grid_cell_size=[1 1 1];
 if prior{ip}.dim(1)>1
-    prior{ip}.S.grid_cell_size(1)=prior{ip}.x(2)-prior{ip}.x(1);
+    prior{ip}.MPS.grid_cell_size(1)=prior{ip}.x(2)-prior{ip}.x(1);
 end
 if prior{ip}.dim(2)>1
-    prior{ip}.S.grid_cell_size(2)=prior{ip}.y(2)-prior{ip}.y(1);
+    prior{ip}.MPS.grid_cell_size(2)=prior{ip}.y(2)-prior{ip}.y(1);
 end
 if prior{ip}.dim(3)>1
-    prior{ip}.S.grid_cell_size(3)=prior{ip}.z(2)-prior{ip}.z(1);
+    prior{ip}.MPS.grid_cell_size(3)=prior{ip}.z(2)-prior{ip}.z(1);
 end
 
 % random seed?
 % set random seed
 if isfield(prior{ip},'seed');
-    prior{ip}.S.rseed=prior{ip}.seed;
+    prior{ip}.MPS.rseed=prior{ip}.seed;
 else
-    prior{ip}.S.rseed=ceil(rand(1).*1e+6);
+    prior{ip}.MPS.rseed=ceil(rand(1).*1e+6);
 end
 
 
@@ -110,10 +122,10 @@ end
 if isfield(prior{ip},'hard_data');
     if ischar(prior{ip}.hard_data)
         % Hard data is provided in file
-        prior{ip}.S.hard_data_filename=prior{ip}.hard_data;
+        prior{ip}.MPS.hard_data_filename=prior{ip}.hard_data;
     else
         % save hard data, and set hard data filename
-        filename_hard=prior{ip}.S.hard_data_filename;
+        filename_hard=prior{ip}.MPS.hard_data_filename;
         if isempty(prior{ip}.hard_data)
             if exist(filename_hard,'file');
                 delete(filename_hard);
@@ -127,7 +139,7 @@ else
     if exist('mps_hard_data_dummy.dat','file');
         try;delete('mps_hard_data_dummy.dat');end
     end
-    prior{ip}.S.hard_data_filename=['mps_hard_data_dummy.dat'];
+    prior{ip}.MPS.hard_data_filename=['mps_hard_data_dummy.dat'];
 end
 
 
@@ -135,11 +147,11 @@ end
 if isfield(prior{ip},'soft_data');
     if ischar(prior{ip}.soft_data)
         % Hard data is provided in file
-        prior{ip}.S.soft_data_filename=prior{ip}.soft_data;
+        prior{ip}.MPS.soft_data_filename=prior{ip}.soft_data;
     else
         % save soft data, and set  soft filename
         try
-            filename_soft=prior{ip}.S.soft_data_filename;
+            filename_soft=prior{ip}.MPS.soft_data_filename;
         catch
             filename_soft='mps_soft.dat';
         end
@@ -149,7 +161,7 @@ if isfield(prior{ip},'soft_data');
 elseif isfield(prior{ip},'soft_data_grid');
     if ischar(prior{ip}.soft_data_grid)
         % Hard data is provided in file
-        prior{ip}.S.soft_data_filename=prior{ip}.soft_data_grid;
+        prior{ip}.MPS.soft_data_filename=prior{ip}.soft_data_grid;
     else
         % convert grid file into [x y z p0 p1 ...] file
         ncat=length(unique(prior{ip}.ti(:)));
@@ -167,7 +179,7 @@ elseif isfield(prior{ip},'soft_data_grid');
             soft_data(:,3+ic)=d(:);
 
         end
-        filename_soft=prior{ip}.S.soft_data_filename;
+        filename_soft=prior{ip}.MPS.soft_data_filename;
         sippi_verbose(sprintf('%s: saving soft data grid to %s',mfilename,filename_soft));
         write_eas(filename_soft,soft_data);
         
@@ -176,7 +188,7 @@ else
     if exist('mps_soft_data_dummy.dat','file');
         try;delete('mps_soft_data_dummy.dat');end
     end
-    prior{ip}.S.soft_data_filename=['mps_soft_data_dummy.dat'];
+    prior{ip}.MPS.soft_data_filename=['mps_soft_data_dummy.dat'];
 end
 
 
@@ -184,7 +196,7 @@ end
 
 
 %% RUN FORWARD
-[m_propose{ip},prior{ip}.S]=mps_cpp(prior{ip}.ti,SIM,prior{ip}.S);
+[m_propose{ip},prior{ip}.MPS]=mps_cpp(prior{ip}.ti,SIM,prior{ip}.MPS);
 
 
 
