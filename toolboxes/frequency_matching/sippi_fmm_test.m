831b4694f5a2113b80a9ab044b9a7dd3c1963ff3
Thomas Mejer Hansen <thomas.mejer.hansen@gmail.com>
2016-05-15 11:23:42 +0200

    --..--

--- toolboxes/frequency_matching/sippi_fmm_test.m
+++ toolboxes/frequency_matching/sippi_fmm_test.m
@@ -1,14 +1,14 @@
 clear all;close all
 
 forward.forward_function='sippi_forward_fmm';
-forward.fmm.N=5;
+forward.fmm.N=9;
 
 % prior;
 ip=1;
 prior{ip}.type='fftma';
 
-prior{ip}.x=1:1:35;
-prior{ip}.y=1:1:40;
+prior{ip}.x=1:1:83;
+prior{ip}.y=1:1:83;
 prior{ip}.m0=0;
 prior{ip}.Cm='1 Sph(5,90,0.2)';
 prior{ip}.d_target=[0 0 0 1 1 1];
@@ -31,12 +31,13 @@ m=sippi_prior(prior);
 [d,forward]=sippi_forward(m,forward);
 
 n_av_count_per_bin=ceil(sum(d{1})./length(d{1}));
-data{1}.nprior=n_av_count_per_bin*10;
+data{1}.nprior=1;
+data{1}.nprior=n_av_count_per_bin*1;
 [logL]=sippi_likelihood(d,data)
 
 %% MCMC
 %prior{1}.seq_gibbs.step=0.01;
-options.mcmc.nite=10000;   % [1] : Number if iterations
-options.mcmc.i_sample=100; % : Number of iterations between saving model to disk
-options.mcmc.i_plot=50;  % [1]: Number of iterations between updating plots
+options.mcmc.nite=1000000;   % [1] : Number if iterations
+options.mcmc.i_sample=5000; % : Number of iterations between saving model to disk
+options.mcmc.i_plot=1000;  % [1]: Number of iterations between updating plots
 [options,data,prior,forward,m_current]=sippi_metropolis(data,prior,forward,options)
