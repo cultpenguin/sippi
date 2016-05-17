826e183f5708ae6a031d48e82f636f42124180a2
Thomas Mejer Hansen <thomas.mejer.hansen@gmail.com>
2016-05-17 12:20:30 +0000

    Frequency Matching Implementation version 1

--- toolboxes/frequency_matching/sippi_fmm_test.m
+++ toolboxes/frequency_matching/sippi_fmm_test.m
@@ -1,36 +1,84 @@
 clear all;close all
 
 forward.forward_function='sippi_forward_fmm';
-%forward.fmm.N=9;
-forward.fmm.N=5;
 
 % prior;
 ip=1;
 prior{ip}.type='fftma';
 
+prior{ip}.x=1:1:125;
+prior{ip}.y=1:1:125;
 %prior{ip}.x=1:1:83;
 %prior{ip}.y=1:1:83;
-prior{ip}.x=1:1:33;
-prior{ip}.y=1:1:33;
+%prior{ip}.x=1:1:33;
+%prior{ip}.y=1:1:33;
 prior{ip}.m0=0;
-prior{ip}.Cm='1 Sph(5,90,0.2)';
-prior{ip}.Cm='1 Sph(.5,90,0.2)';
+prior{ip}.Cm='1 Sph(10,90,0.001)'; % horizontal
+%prior{ip}.Cm='1 Sph(.5,90,0.2)';
 prior{ip}.d_target=[0 0 0 0 0 0 0 1 1 1];
 [m_ref,prior]=sippi_prior(prior);
 
-% REFERENCE DATA
+define_template=1;
+if define_template==1
+    T=[0 0 0;
+    0 1 0;
+    0 -1 0;
+    0 2 0;
+    0 -2 0;
+    0 3 0;
+    0 -3 0];
+    forward.fmm.T=T;
+elseif define_template==2
+  T=[0 0 0;
+    1 0 0;
+    -1 0 0;
+    2 0 0;
+    -2 0 0;
+    3 0 0;
+    -3 0 0];
+    forward.fmm.T=T;
+elseif define_template==3
+  T=[0 0 0;
+    0 1 0;
+    0 -1 0;
+    0 2 0;
+    0 -2 0;
+    0 3 0;
+    -3 0 0;
+    1 0 0;
+    -1 0 0;
+    2 0 0;
+    -2 0 0;
+    3 0 0;
+    -3 0 0];
+    forward.fmm.T=T;
+elseif define_template==4
+    T=[0 0 0];
+    forward.fmm.T=T;
+else
+    % make default template
+    %forward.fmm.N=9;
+    forward.fmm.N=5;
+end
+
+
+%% REFERENCE DATA
 use_ti=1;
 if use_ti==1;
     TI=channels;
     TI=TI(2:2:end,2:2:end);
     m_ref{1}=TI;
 end
+tic
+profile on
 [d,forward]=sippi_forward_fmm(m_ref,forward);
-
+profile report
+profile off
+toc
 data{1}.d_obs=d{1};
 data{1}.noise_model='sippi_likelihood_fmm';
 
-% TEST
+%% TEST
 m=sippi_prior(prior);
 [d,forward]=sippi_forward(m,forward);
 
