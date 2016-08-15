12fdc6f9e8fc0c9ae466ac3f4fe8eefbbd913804
Thomas Mejer Hansen <tmeha@nbi.ku.dk>
2016-08-15 14:41:42 +0200

    Example: Figure 2 in Sambridge, 2014

--- plotting/sippi_plot_data.m
+++ plotting/sippi_plot_data.m
@@ -40,7 +40,7 @@ options.null='';
 
 if nargin>3
     if isfield(options,'sippi_plot_data_function');
-        sippi_verbose(sprintf('%s : Using ''%s'' instead of sippi_plot_data',mfilename,options.sippi_plot_data_function),-2)
+        sippi_verbose(sprintf('%s : Using ''%s'' instead of sippi_plot_data',mfilename,options.sippi_plot_data_function),1);
         if nargin==1;
             feval(options.sippi_plot_data_function,d);
         else
