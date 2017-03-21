cf7fb5c6f0afb795a4809338e944ee8868dafe8b
KSCordua <knudcordua@gmail.com>
2017-03-21 13:08:45 +0100

    More information in help

--- toolboxes/frequency_matching/sippi_likelihood_fmm.m
+++ toolboxes/frequency_matching/sippi_likelihood_fmm.m
@@ -1,18 +1,35 @@
 % sippi_likelihood_multinomial: Compute likelihood using the multinomial function
 %
-% Call
+% Call:
 %   [logL,L,data]=sippi_likelihood_multinomial(d,data);
+%
+% Input parameter:
+%    d{id}; Frequency distribution to be evaluated. 
+%    data{id}.d_obs; Observed frequency distribution.
+%    data{id}.nprior; Prior frequency distribution.
+%
+% This likelihood uses the multinomial distribution to calculate the
+% probability of some (e.g. pattern) frequency distribution given some
+% observed frequency distribution and a prior frequency distribution.
 %
 % See also sippi_likelihood
 %
-function [logL,logL_all,data]=sippi_likelihood_multinom(d,data,id_array);
+% For more details on the theory see:
+% Cordua et al., 2015. Improving the Pattern Reproducibility of Multiple-Point-Based Prior
+% Models Using Frequency Matching. Mathematical Geosciences. April 2015,
+% Volume 47, Issue 3, pp 317?343. DOI: 10.1007/s11004-014-9531-4.1111
+
+
+function [logL,logL_all,data]=sippi_likelihood_fmm(d,data,id_array)
 if  nargin<3
-    id_array=1:length(d);
+    LL=length(d);
+    id_array=1:LL;
 end
 
+logL_all=zeros(1,L);
 for id=id_array
     
-    logL_all(id)=0;
+    %logL_all(id)=0;
 
     if ~isfield(data{id},'nprior');
         data{id}.nprior=0;
