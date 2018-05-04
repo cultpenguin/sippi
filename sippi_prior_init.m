39b386269f2754335f4790c2b56c8d00591bf8b9
Thomas Mejer Hansen <thomas.mejer.hansen@gmail.com>
2018-05-04 23:33:41 +0200

    ..

--- sippi_prior_init.m
+++ sippi_prior_init.m
@@ -355,7 +355,7 @@ for im=im_array
     
     %% TARGET DIST - CHECK THAT NORMAL SCORE HASE BEEN PERFORMED
     if isfield(prior{im},'d_target')
-        if ~isfield(prior{im},'o_nscore');
+        if ~isfield(prior{im},'o_nscore')&&~iscell(prior{im}.d_target);
             [d_nscore,prior{im}.o_nscore]=nscore(prior{im}.d_target,1,1);
             sippi_verbose(sprintf('%s: performed normal score transformation of prior{%d}.d_target to prior{%d}.o_nscore',mfilename,im,im));
         end
