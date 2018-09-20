80ae598d91fcf769ba7da046d15620cd34a1250a
Thomas Mejer Hansen <thomas.mejer.hansen@gmail.com>
2018-09-20 11:28:40 +0200

    sippi_metropolis_gibbs: 2D gibbs Now works with annealing and temperature...

--- sippi_metropolis_gibbs_random_iteration_2d.m
+++ sippi_metropolis_gibbs_random_iteration_2d.m
@@ -52,7 +52,14 @@ for ic=1:NC;
         logL=zeros(1,mcmc.gibbs.Nm);
         pPrior=zeros(1,mcmc.gibbs.Nm);
         for im=1:mcmc.gibbs.Nm;
-            [m_test{im},prior_test{im}]=sippi_prior(prior,m);
+            if im==1;
+                % make sure current model is part of the tested models
+                m_test{im}=C{ic}.m_current;
+                prior_test{im}=C{ic}.prior_current;
+            else
+                [m_test{im},prior_test{im}]=sippi_prior(prior,m);
+            end
+            %[m_test{im},prior_test{im}]=sippi_prior(prior,m);
             [d_test{im},forward]=sippi_forward(m_test{im},C{ic}.forward,prior_test{im},C{ic}.data);
             [logL(im)]=sippi_likelihood(d_test{im},C{ic}.data);
             
@@ -68,8 +75,17 @@ for ic=1:NC;
                 logPrior(iip,:) =  log(normpdf(m_arr(iip,:),C{ic}.prior_current{ip(iip)}.m0,C{ic}.prior_current{ip(iip)}.std));
             end
         end
+        
+         if isfield(mcmc,'anneal')
+            [C{ic}.T_fac,mcmc]=sippi_anneal_temperature(i,mcmc,C{ic}.prior_current);
+            T=C{ic}.T_fac.*C{ic}.T;
+        else
+            T=C{ic}.T; % only use the 'base' temperature.
+        end
+        
         logPost = logL + sum(logPrior);
-        pPost=exp(logPost-max(logPost));
+        logPost_norm = logPost-max(logPost);
+       
         
         %% Nearest Neighbor interpolate
         if isfield(mcmc.gibbs,'Nn1');
@@ -90,15 +106,21 @@ for ic=1:NC;
         [imm1,imm2]=meshgrid(1:Nn1,1:Nn2);
         i_logPost_G=griddata(im1,im2,logPost,imm1,imm2,'nearest');
         %logPost_G=griddata(m_arr(1,:),m_arr(2,:),logPost,mm1,mm2,'nearest');
-        P=exp(i_logPost_G-max(i_logPost_G(:)));
+        P=exp( (1/T)* (i_logPost_G-max(i_logPost_G(:))) );
         
         
         % sample from 2D pdf
         [m1_sim,m2_sim,ir_1,ir_2]=sample_from_2d_pdf(P,m1,m2);
-        % find the corresponding prior real
-        im_use = find(logPost==i_logPost_G(ir_2,ir_1));
         
-        
+        % Find closest match
+        w=max(m_arr')-min(m_arr');
+        %dx = max
+        for im=1:mcmc.gibbs.Nm;
+            dis(im) = sqrt( ((m_test{im}{ip(1)}-m1_sim)./(w(1)))^2 + ((m_test{im}{ip(2)}-m2_sim)./(w(2)))^2 );
+        end
+        im_use=find(dis==min(dis));
+        im_use=im_use(1);
+        m_test{im_use};
         
         % move to current model
         prior_ref=C{ic}.prior_current;
@@ -132,11 +154,14 @@ for ic=1:NC;
             subplot(1,2,2);
             imagesc(m1,m2,P);colormap(1-gray)
             hold on
-            plot(m1(ir_1),m2(ir_2),'r*')
+            plot(m1(ir_1),m2(ir_2),'r.')
+            plot([m{ip(1)},C{ic}.m_current{ip(1)}],[m{ip(2)},C{ic}.m_current{ip(2)}],'r--')
+            plot(C{ic}.m_current{ip(1)},C{ic}.m_current{ip(2)},'r*')
+            plot(m{ip(1)},m{ip(2)},'g*')
             hold off
             xlabel(sprintf('m_%d',ip(1)))
             ylabel(sprintf('m_%d',ip(2)))
-            title(sprintf('f(m_%d,m_%d | not) at iteration %d',ip(1),ip(2),mcmc.i))
+            title(sprintf('f(m_%d,m_%d | not) at iteration %d, T=%g',ip(1),ip(2),mcmc.i, T))
             
             
             drawnow;
